1e7dd8ac57542449bacbfc2b5e9f62d10fb61f88
Udimberto Silvestre Junior <udi@ingresse.com>
2019-07-25 15:38:21 -0300

    Update: 'Modal' handle click behavio;

---
+++ src/components/Modal/ModalOverlayStyles.js
@@ -0,0 +1,20 @@
+/* Packages */
+import styled from '@emotion/styled';
+
+/* Constants */
+import { ZINDEX } from '../../constants';
+
+/* Modal Overlay Styles */
+const ModalOverlayStyled = styled.div`
+    position: fixed;
+    z-index : ${ZINDEX.MODAL - 1};
+    top     : 0;
+    right   : 0;
+    bottom  : 0;
+    left    : 0;
+
+    ${props => props.styles};
+`;
+
+/* Exporting */
+export default ModalOverlayStyled;
