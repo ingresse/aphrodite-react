860c05f56d6ace3ac0d2fcf00e3161108a478bc1
Udimberto Silvestre Junior <udi@ingresse.com>
2019-08-29 16:17:44 -0300

    Update: 'Action Bar' background theme;

--- dist/constants/zindex.constants.js
+++ dist/constants/zindex.constants.js
@@ -5,6 +5,7 @@ exports.ZINDEX = void 0;
 
 /* z-index components values */
 var ZINDEX = {
+  ACTION_BAR: 100,
   NAVBAR: 9000,
   MODAL: 10000,
   TOAST: 10010
--- src/constants/zindex.constants.js
+++ src/constants/zindex.constants.js
@@ -1,6 +1,7 @@
 /* z-index components values */
 export const ZINDEX = {
-    NAVBAR: 9000,
-    MODAL : 10000,
-    TOAST : 10010,
+    ACTION_BAR: 100,
+    NAVBAR    : 9000,
+    MODAL     : 10000,
+    TOAST     : 10010,
 };
